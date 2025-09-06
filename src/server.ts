43b77356e842585b9391d3990e75ae06822c1cbb
rhapsocraft <axciom545@gmail.com>
2025-09-07 05:02:36 +0800

    feat: adds initial project setup

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.SERVER_PORT || 3000;
+
+app.listen(port, () => {
+    console.info(`Server is running on port ${port}`);
+});
