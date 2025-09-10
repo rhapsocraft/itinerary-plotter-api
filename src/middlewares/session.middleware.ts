f6bd7d121e761878491a2f769c8322450c4fcc4f
rhapsocraft <axciom545@gmail.com>
2025-09-11 03:43:10 +0800

    feat: enables persistence of sessions

--- src/middlewares/session.middleware.ts
+++ src/middlewares/session.middleware.ts
@@ -1,9 +1,18 @@
 import { RequestHandler } from 'express';
 import session from 'express-session';
+import sessionConnector from 'connect-pg-simple';
 import { env } from '@/config/environment';
 
 export function sessionMiddleware(): RequestHandler {
   return session({
+    store: new (sessionConnector(session))({
+      tableName: 'user_sessions',
+      conString: env.DATABASE_URL,
+      createTableIfMissing: true,
+    }),
+    cookie: {
+      maxAge: 86_400_000,
+    },
     secret: env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
